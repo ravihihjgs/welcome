b9e3b3e35e221649a11288e913d187a1867dae68
Ravi Ranjan Kumar <ravi1141.be21@chitkara.edu.in>
2024-10-16 11:16:55 +0530

    Update Calculator1.java

--- Calculator1.java
+++ Calculator1.java
@@ -1,8 +1,6 @@
 public class Calculator1 {
     public static void main(String[] args) {
-        int a=10;
-        int b=5;
-        int c=a+b;
-        System.out.print(c);
+        
+        System.out.print("Welcome");
     }
 }
